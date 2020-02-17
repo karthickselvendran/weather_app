fe3a3ee3e5fec573b89034f2cbdf2db30a366d87
Karthick S <selvendrankarthick@gmail.com>
2020-02-18 00:25:50 +0530

    Updates

---
+++ precache-manifest.2a13d303b493e2a052342d9cf03004cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600d9ec45e8ab68e6346ea0ce8e8b95a",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "a9059d7c1334a5beeaf2",
+    "url": "/weather_app/static/css/2.760b8bd1.chunk.css"
+  },
+  {
+    "revision": "60908c68921c11eabcdb",
+    "url": "/weather_app/static/css/main.06b5e1d3.chunk.css"
+  },
+  {
+    "revision": "a9059d7c1334a5beeaf2",
+    "url": "/weather_app/static/js/2.b5899ac3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather_app/static/js/2.b5899ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60908c68921c11eabcdb",
+    "url": "/weather_app/static/js/main.9454a44f.chunk.js"
+  },
+  {
+    "revision": "c59ec78c84f5cb78b4aa",
+    "url": "/weather_app/static/js/runtime-main.42402ec5.js"
+  }
+]);
